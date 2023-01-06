814c1d527b7900e19488bf459ef851058d44c23c
Pavel <pateterin@gmail.com>
2023-01-06 16:31:55 +0300

    condition

---
+++ js/condition.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// if (1) {
+//   console.log('Ok')
+// } else {
+//   console.log('error')
+// }
+
+// if (num < 49) {
+//   console.log('error')
+// } else if (num > 100) {
+//   console.log('A lot')
+// } else {
+//   console.log('OK!')
+// }
+
+// num === 50
+//   ? console.log('Ok!')
+//   : console.log('error')
+
+const num = '50'
+
+switch (num) {
+  case '49':
+    console.log('Errors!')
+    break
+  case '49':
+    console.log('Errors!')
+    break
+  case '50':
+    console.log('Good')
+    break
+  default:
+    console.log('WTF')
+    break
+}
